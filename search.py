1f29def18902353b37be89ed7549def7d6fee1ca
EricTheRed12 <eric12redman@yahoo.com>
2021-03-11 19:44:57 -0700

    Foudn a website thats not google

--- search.py
+++ search.py
@@ -1,28 +1,17 @@
-import urllib.request
+import requests
 import time
-import numpy as np
+
 from bs4 import BeautifulSoup
 
 word = input()
-search = f"{word}%20definition"
 
-url = f"https://www.google.com/search?&q={search}"
+url = "http://www.mso.anu.edu.au/~ralph/OPTED/v003/wb1913_a.html"
 
-req = urllib.request.Request(
-    url,
-    data=None,
-    headers={
-        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36'
-    }
-)
+r = requests.get(url)#urllib.request.urlopen(url).read()
+print(r.status_code)
 
-r = urllib.request.urlopen(req).read()
+result = BeautifulSoup(r.text, "html.parser")
 
-result = BeautifulSoup(r, "html.parser")
-result = str(result.get_text())
-result = result[result.find(word+";"):result.find("Definitions")]
-result = result.split(';')
-print(result)
-#print(result.prettify())
+print(result.prettify())
 
-time.sleep(15)
+#time.sleep(15)
