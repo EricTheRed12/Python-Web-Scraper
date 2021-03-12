36b881a94d922039a582bcf6a61104b7472e03b6
EricTheRed12 <eric12redman@yahoo.com>
2021-03-11 19:26:49 -0700

    Python web scrapping. Canceled.  I dont wish to offend google yet

--- search.py
+++ search.py
@@ -1,7 +1,28 @@
-from googlesearch import search
+import urllib.request
+import time
+import numpy as np
+from bs4 import BeautifulSoup
 
-# to search
-query = "Geeksforgeeks"
+word = input()
+search = f"{word}%20definition"
 
-for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-    print(j) 
+url = f"https://www.google.com/search?&q={search}"
+
+req = urllib.request.Request(
+    url,
+    data=None,
+    headers={
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.72 Safari/537.36'
+    }
+)
+
+r = urllib.request.urlopen(req).read()
+
+result = BeautifulSoup(r, "html.parser")
+result = str(result.get_text())
+result = result[result.find(word+";"):result.find("Definitions")]
+result = result.split(';')
+print(result)
+#print(result.prettify())
+
+time.sleep(15)
